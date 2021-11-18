1aa883f0f3804f4aab5a66af8973c398dccc1754
Hamilton Gonçalves junior <hamiltonjr2010@gmail.com>
2021-11-18 17:18:45 -0300

    First commit.

---
+++ Hello/src/com/gmail/hamiltonjr2010/Main.java
@@ -0,0 +1,12 @@
+package com.gmail.hamiltonjr2010;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		Gato g = new Gato("Naila", "mel", 2);
+		System.out.println(g);
+		
+	}
+
+}
